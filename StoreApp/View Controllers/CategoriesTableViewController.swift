926152e5923926356e845b8528f1afd00bd8fcdc
sochubert <sochubert@gmail.com>
2023-02-23 16:22:53 +0900

    Displaying all Categories

--- StoreApp/View Controllers/CategoriesTableViewController.swift
+++ StoreApp/View Controllers/CategoriesTableViewController.swift
@@ -10,10 +10,59 @@ import UIKit
 
 class CategoriesTableViewController: UITableViewController {
     
+    private var client = StoreHTTPClient()
+    private var categories: [Category] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white 
+        view.backgroundColor = UIColor.white
+        title = "Categories"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CategoryTableViewCell")
+        
+        Task {
+            await populateCategories()
+            tableView.reloadData()
+        }
     }
     
+    private func populateCategories() async {
+        do {
+            categories = try await client.getAllCategories()
+        } catch {
+            // show error in alert
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryTableViewCell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        
+        let category = categories[indexPath.row]
+        
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = category.name
+        
+        if let url = URL(string: category.image) {
+            DispatchQueue.global().async {
+                if let data = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)
+                        configuration.image = image
+                        configuration.imageProperties.maximumSize = CGSize(width: 75, height: 75)
+                        cell.contentConfiguration = configuration
+                    }
+                }
+            }
+        }
+        
+        cell.contentConfiguration = configuration
+        
+        return cell
+    }
 }
 
